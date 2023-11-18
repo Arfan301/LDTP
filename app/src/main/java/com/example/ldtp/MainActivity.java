e3f1b21c94b3ec8455cacdb0f860056f189de9f4
Arfan301 <maulana.arfan301@gmail.com>
2023-11-18 21:42:47 +0700

    Second commit added: - UI/UX = Buttons, EditText(display inputted) - functionality = dispay inputs after input
    updates/changes:
    adkCompiler version, etc to get rid of the emulator error

--- app/src/main/java/com/example/ldtp/MainActivity.java
+++ app/src/main/java/com/example/ldtp/MainActivity.java
@@ -3,12 +3,81 @@ package com.example.ldtp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.userInput);
+        display.setShowSoftInputOnFocus(false);
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getString(R.string.display).equals(display.getText().toString())){
+                    display.setText("");
+                }
+            }
+        });
+    }
+
+    private void updateText(String addInput){
+        String oldStr = display.getText().toString();
+        int cursorPos = display.getSelectionStart();
+        String leftStr = oldStr.substring(0, cursorPos);
+        String rightStr = oldStr.substring(cursorPos);
+
+        if(getString(R.string.display).equals(display.getText().toString())){
+            display.setText(addInput);
+        }else {
+            display.setText(String.format("%s%s%s", leftStr, addInput, rightStr));
+//            display.setSelection(cursorPos + 1);
+        }
+    }
+
+    public void zeroBTN(View view){
+        updateText("0");
+    }
+
+    public void oneBTN(View view){
+        updateText("1");
+    }
+
+    public void twoBTN(View view){
+        updateText("2");
+    }
+
+    public void threeBTN(View view){
+        updateText("3");
+    }
+
+    public void fourBTN(View view){
+        updateText("4");
+    }
+
+    public void fiveBTN(View view){
+        updateText("5");
+    }
+
+    public void sixBTN(View view){
+        updateText("6");
+    }
+
+    public void sevenBTN(View view){
+        updateText("7");
+    }
+
+    public void eightBTN(View view){
+        updateText("8");
+    }
+
+    public void nineBTN(View view){
+        updateText("9");
     }
 }

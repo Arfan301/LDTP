61f513d9743831aa3355a86fb563839120c5c7df
Arfan301 <maulana.arfan301@gmail.com>
2023-11-25 16:51:19 +0700

    Fourth Commit: Added Splash Screen and Bottom navigation Bar

--- app/src/main/java/com/example/ldtp/MainActivity.java
+++ app/src/main/java/com/example/ldtp/MainActivity.java
@@ -1,22 +1,55 @@
 package com.example.ldtp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.text.SpannableStringBuilder;
 import android.view.View;
 import android.widget.EditText;
 
+import com.example.ldtp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     private EditText display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new MainFragment());
 
-        display = findViewById(R.id.userInput);
-        display.setShowSoftInputOnFocus(false);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()){
+                case R.id.main:
+                    replaceFragment(new MainFragment());
+                    break;
+
+//                case R.id.history:
+//                    replaceFragment(new HistoryFragment());
+//                    break;
+//
+//                case R.id.profile:
+//                    replaceFragment(new NotifFragment());
+//                    break;
+            }
+            return true;
+        });
+
+
+//        display = findViewById(R.id.userInput);
+//        display.setShowSoftInputOnFocus(false);
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frameId,fragment);
+        ft.commit();
     }
 
     private void updateText(String addInput){

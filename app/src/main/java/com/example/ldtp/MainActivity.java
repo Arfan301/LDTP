ae20e454b49bc26cc53259875d777ca886f7eb88
Arfan301 <maulana.arfan301@gmail.com>
2023-11-27 10:34:10 +0700

    Resolved Crash due to fragment cannot detect the onClick function.

--- app/src/main/java/com/example/ldtp/MainActivity.java
+++ app/src/main/java/com/example/ldtp/MainActivity.java
@@ -39,10 +39,6 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
-
-
-//        display = findViewById(R.id.userInput);
-//        display.setShowSoftInputOnFocus(false);
     }
 
     private void replaceFragment(Fragment fragment){
@@ -50,76 +46,5 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction ft = fm.beginTransaction();
         ft.replace(R.id.frameId,fragment);
         ft.commit();
-    }
-
-    private void updateText(String addInput){
-        String oldStr = display.getText().toString();
-        int cursorPos = display.getSelectionStart();
-        String leftStr = oldStr.substring(0, cursorPos);
-        String rightStr = oldStr.substring(cursorPos);
-
-        if(getString(R.string.display).equals(display.getText().toString())){
-            display.setText(addInput);
-            display.setSelection(cursorPos + 1);
-        }else {
-            display.setText(String.format("%s%s%s", leftStr, addInput, rightStr));
-            display.setSelection(cursorPos + 1);
-        }
-    }
-
-    public void zeroBTN(View view){
-        updateText("0");
-    }
-
-    public void oneBTN(View view){
-        updateText("1");
-    }
-
-    public void twoBTN(View view){
-        updateText("2");
-    }
-
-    public void threeBTN(View view){
-        updateText("3");
-    }
-
-    public void fourBTN(View view){
-        updateText("4");
-    }
-
-    public void fiveBTN(View view){
-        updateText("5");
-    }
-
-    public void sixBTN(View view){
-        updateText("6");
-    }
-
-    public void sevenBTN(View view){
-        updateText("7");
-    }
-
-    public void eightBTN(View view){
-        updateText("8");
-    }
-
-    public void nineBTN(View view){
-        updateText("9");
-    }
-
-    public void clearBTN(View view){
-        display.setText("");
-    }
-
-    public void backBTN(View view){
-        int cursorPos = display.getSelectionStart();
-        int textLen = display.getText().length();
-
-        if (cursorPos != 0 &&  textLen != 0){
-            SpannableStringBuilder selection = (SpannableStringBuilder) display.getText();
-            selection.replace(cursorPos - 1, cursorPos, "");
-            display.setText(selection);
-            display.setSelection(cursorPos - 1);
-        }
     }
 }
--- app/src/main/java/com/example/ldtp/MainFragment.java
+++ app/src/main/java/com/example/ldtp/MainFragment.java
@@ -4,61 +4,180 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.SpannableStringBuilder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MainFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MainFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
-        MainFragment fragment = new MainFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private EditText display;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        display = view.findViewById(R.id.userInput);
+        display.setShowSoftInputOnFocus(false);
+
+        view.findViewById(R.id.button0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zeroBTN(view);
+            }
+        });
+
+        view.findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                oneBTN(view);
+            }
+        });
+
+        view.findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                twoBTN(view);
+            }
+        });
+
+        view.findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                threeBTN(view);
+            }
+        });
+
+        view.findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fourBTN(view);
+            }
+        });
+
+        view.findViewById(R.id.button5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fiveBTN(view);
+            }
+        });
+
+        view.findViewById(R.id.button6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sixBTN(view);
+            }
+        });
+
+        view.findViewById(R.id.button7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sevenBTN(view);
+            }
+        });
+
+        view.findViewById(R.id.button8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eightBTN(view);
+            }
+        });
+
+        view.findViewById(R.id.button9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nineBTN(view);
+            }
+        });
+
+        view.findViewById(R.id.buttonBackspace).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                backBTN(view);
+            }
+        });
+
+        view.findViewById(R.id.buttonClear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearBTN(view);
+            }
+        });
+
+        return view;
+    }
+
+    private void updateText(String addInput){
+        display.findViewById(R.id.userInput);
+        String oldStr = display.getText().toString();
+        int cursorPos = display.getSelectionStart();
+        String leftStr = oldStr.substring(0, cursorPos);
+        String rightStr = oldStr.substring(cursorPos);
+
+        if(getString(R.string.display).equals(display.getText().toString())){
+            display.setText(addInput);
+            display.setSelection(cursorPos + 1);
+        }else {
+            display.setText(String.format("%s%s%s", leftStr, addInput, rightStr));
+            display.setSelection(cursorPos + 1);
+        }
+    }
+
+
+
+    public void zeroBTN(View view){
+        updateText("0");
+    }
+
+    public void oneBTN(View view){
+        updateText("1");
+    }
+
+    public void twoBTN(View view){
+        updateText("2");
+    }
+
+    public void threeBTN(View view){
+        updateText("3");
+    }
+
+    public void fourBTN(View view){
+        updateText("4");
+    }
+
+    public void fiveBTN(View view){
+        updateText("5");
+    }
+
+    public void sixBTN(View view){
+        updateText("6");
+    }
+
+    public void sevenBTN(View view){
+        updateText("7");
+    }
+
+    public void eightBTN(View view){
+        updateText("8");
+    }
+
+    public void nineBTN(View view){
+        updateText("9");
+    }
+
+    public void clearBTN(View view){
+        display.setText("");
+    }
+
+    public void backBTN(View view){
+        int cursorPos = display.getSelectionStart();
+        int textLen = display.getText().length();
+
+        if (cursorPos != 0 &&  textLen != 0){
+            SpannableStringBuilder selection = (SpannableStringBuilder) display.getText();
+            selection.replace(cursorPos - 1, cursorPos, "");
+            display.setText(selection);
+            display.setSelection(cursorPos - 1);
+        }
     }
 }
